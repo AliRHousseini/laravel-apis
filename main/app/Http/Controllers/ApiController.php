8b6193949980a689a1556fcf7051b46ec49fee91
Ali Husseini <macbookpro@Apples-MacBook-Pro.local>
2022-09-30 18:19:25 +0300

    routes: adding the route path

--- main/app/Http/Controllers/ApiController.php
+++ main/app/Http/Controllers/ApiController.php
@@ -7,4 +7,20 @@ use Illuminate\Http\Request;
 class ApiController extends Controller
 {
     //
+    function getUsers(){
+        dd("asdasdasd");
+        return "HI from here";
+    }
+
+    function addUser(Request $request){
+        $name = $request->name;
+        $age = $request ->age;
+
+        return response()->json([
+            "status" => "Success",
+            "message" => $age
+        ]);
+    }
+
+
 }
